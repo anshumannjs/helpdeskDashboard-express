213dd0b09ddeb2fbfe1adb19004024be22bd7e17
anshuman <anshumannarendrajitsingh@gmail.com>
2024-08-03 23:21:43 +0530

    trying to fix cors 5

--- src/index.js
+++ src/index.js
@@ -15,6 +15,15 @@ app.use(cors({
 }))
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Origin');
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+    }
+    next();
+  });
 app.use("/auth", auth)
 app.use("/ticket", ticket)
 
