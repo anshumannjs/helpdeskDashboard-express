167b3f2733da4c7d2e09dcdf238621f4dc619fca
anshuman <anshumannarendrajitsingh@gmail.com>
2024-08-03 23:16:26 +0530

    trying to fix cors 4

--- src/index.js
+++ src/index.js
@@ -8,8 +8,11 @@ const User = require("./database/UserSchema")
 require("./database/index")
 
 const app=express()
-app.use(cors())
-app.options("*", cors())
+app.use(cors({
+    headers: ["Content-Type"],
+    credentials: true,
+    origin: "*"
+}))
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use("/auth", auth)
